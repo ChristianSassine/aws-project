003e6e4384fd61661a1c02e4a97d8f0e0da5fe69
Christian <71024958+ChristianSassine@users.noreply.github.com>
2024-11-28 05:52:03 -0500

    deploy with asyncio

--- deploy/main.py
+++ deploy/main.py
@@ -2,10 +2,11 @@ import launch
 from cleanup import cleanup
 import sys
 from constants import *
+import asyncio
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if args and args[0] == "cleanup":
         cleanup()
     elif not args:
-        instance = launch.deploy()
+        asyncio.run(launch.deploy())
