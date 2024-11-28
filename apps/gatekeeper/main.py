a42eba79860faed70e5c2f1a1f164499705a4b01
Christian <71024958+ChristianSassine@users.noreply.github.com>
2024-11-28 05:57:18 -0500

    Update gatekeeper to forward requests to trusted host

--- apps/gatekeeper/main.py
+++ apps/gatekeeper/main.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI, Request
 import requests
 import uvicorn
+import json
+from http import HTTPStatus
 
 # Create FastAPI app
 app = FastAPI()
 
-TRUSTED_HOST = ""
+with open("trusted_host.json", "r") as f:
+    trusted_host_data = json.load(f)
+    trusted_host_ip = trusted_host_data["Private IP"]
+
+@app.get("/health", status_code=HTTPStatus.OK)
+async def health_check(request: Request):
+    return
 
 @app.get("/")
 async def send_trusted_host_get(request: Request):
-    return treat_message(request)
+    return await treat_message(request)
+
 
 @app.post("/")
-async def send_trusted_host_get(request: Request):
-    return treat_message(request)
+async def send_trusted_host_post(request: Request):
+    return await treat_message(request)
 
 async def treat_message(request: Request):
     # Get the instance with the smallest response time
@@ -22,21 +31,22 @@ async def treat_message(request: Request):
     # Forward the request to the selected server
     response = requests.request(
         method=request.method,
-        url=f"http://{TRUSTED_HOST}",
+        url=f"http://{trusted_host_ip}",
         headers=request.headers,
         data=await request.body(),
         params=request.query_params,
     )
-    
+
     # Return the response from the backend server
     print(
-        f"Received response from {TRUSTED_HOST}: {response.status_code} - {response.content}"
+        f"Received response from {trusted_host_ip}: {response.status_code} - {response.content}"
     )
     return {
         "status_code": response.status_code,
         "body": response.json(),
-        "headers": dict(response.headers)
+        "headers": dict(response.headers),
     }
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
