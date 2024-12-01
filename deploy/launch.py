6b8b0397b6e271268c5ff8cedf6f08270f4e18a6
Christian <71024958+ChristianSassine@users.noreply.github.com>
2024-12-01 05:15:53 -0500

    Add mysql cluster setup

--- deploy/launch.py
+++ deploy/launch.py
@@ -6,7 +6,6 @@ from infra import *
 from remote import bootstrap_instance, File
 from health_check import health_check_instances
 
-
 async def deploy():
     # Create configs directory
     os.makedirs(get_path(ROOT_CONFIGS_PATH), exist_ok=True)
@@ -22,7 +21,7 @@ async def deploy():
     )
 
     ## Create Gatekeeper
-    gatekeeper = create_ec2_instances("t2.micro", 1, KEY_PAIR_NAME, outside_grp_id)
+    gatekeeper = create_ec2_instances("t2.large", 1, KEY_PAIR_NAME, outside_grp_id)
 
     ## Extract instance's information
     gatekeeper_info = get_instances_info(gatekeeper)[0]
@@ -35,7 +34,7 @@ async def deploy():
     )
 
     ## Create Trusted Host
-    trusted_host = create_ec2_instances("t2.micro", 1, KEY_PAIR_NAME, trusted_grp_id)
+    trusted_host = create_ec2_instances("t2.large", 1, KEY_PAIR_NAME, trusted_grp_id)
 
     ## Extract instance's information
     trusted_host_info = get_instances_info(trusted_host)[0]
@@ -47,23 +46,40 @@ async def deploy():
         INTERNAL_SECURITY_GROUP_NAME, get_outside_ip_permissions()
     )
 
-    ## Create Trusted Proxy
-    proxy = create_ec2_instances("t2.micro", 1, KEY_PAIR_NAME, internal_grp_id)
+    ## Create Proxy
+    proxy = create_ec2_instances("t2.large", 1, KEY_PAIR_NAME, internal_grp_id)
 
     ## Extract instance's information
     proxy_info = get_instances_info(proxy)[0]
     write_file(PROXY_INFO_PATH, json.dumps(proxy_info))
 
-    # TODO: Add cluster setup
+    ## Create Mysql Workers
+    mysql_workers = create_ec2_instances("t2.micro", 2, KEY_PAIR_NAME, internal_grp_id)
+
+    ## Extract instances' informations
+    mysql_workers_info = get_instances_info(mysql_workers)
+    write_file(WORKERS_INFO_PATH, json.dumps(mysql_workers_info))
+
+    print(mysql_workers_info)
+
+    ## Create Mysql Manager
+    mysql_manager = create_ec2_instances("t2.micro", 1, KEY_PAIR_NAME, internal_grp_id)
+    
+    ## Extract instance's informations
+    mysql_manager_info = get_instances_info(mysql_manager)[0]
+    write_file(MANAGER_INFO_PATH, json.dumps(mysql_manager_info))
 
     # Bootstrap the instances
-    setup_instances(gatekeeper_info, trusted_host_info, proxy_info)
+    setup_instances(gatekeeper_info, trusted_host_info, proxy_info, mysql_workers_info, mysql_manager_info)
 
     # Health check to make sure all the servers are active
-    instances_info = [gatekeeper_info, trusted_host_info]  # TODO: Add the rest
+    instances_info = [gatekeeper_info, trusted_host_info, proxy_info, mysql_manager_info] + mysql_workers_info
     await health_check_instances(instances_info)
 
     # Modify the security groups to tighten security
+    tighten_security_groups(trusted_grp_id, internal_grp_id, gatekeeper_info, proxy_info, trusted_host_info, mysql_manager_info, mysql_workers_info)
+
+def tighten_security_groups(trusted_grp_id, internal_grp_id, gatekeeper_info, proxy_info, trusted_host_info, mysql_manager_info, mysql_workers_info):
     modify_security_group_permissions(
         trusted_grp_id,
         get_secure_ip_permissions(
@@ -80,45 +96,67 @@ async def deploy():
             [
                 proxy_info[InstanceInfo.PRIVATE_IP.value],
                 trusted_host_info[InstanceInfo.PRIVATE_IP.value],
-            ]
+                mysql_manager_info[InstanceInfo.PRIVATE_IP.value],
+            ] + [worker_info[InstanceInfo.PRIVATE_IP.value] for worker_info in mysql_workers_info]
         ),
-    )  # TODO: Add the clusters
+    )
 
 
-def setup_instances(gatekeeper_info, trusted_host_info, proxy_info):
+def setup_instances(gatekeeper_info, trusted_host_info, proxy_info, workers_info, manager_info):
     bootstrap_instance(
         get_path(KEY_PAIR_PATH),
         gatekeeper_info[InstanceInfo.PUBLIC_IP.value],
         [
-            File(get_path(BOOTSTRAP_PATH), "bootstrap.sh"),
+            File(get_path(APP_BOOTSTRAP_PATH), "bootstrap.sh"),
             File(get_path(GATEKEEPER_APP_PATH), "main.py"),
             File(get_path(TRUSTED_HOST_INFO_PATH), "trusted_host.json"),
         ],
         gatekeeper_info[InstanceInfo.ID.value],
-        True,
     )
 
     bootstrap_instance(
         get_path(KEY_PAIR_PATH),
         trusted_host_info[InstanceInfo.PUBLIC_IP.value],
         [
-            File(get_path(BOOTSTRAP_PATH), "bootstrap.sh"),
+            File(get_path(APP_BOOTSTRAP_PATH), "bootstrap.sh"),
             File(get_path(TRUSTED_HOST_APP_PATH), "main.py"),
             File(get_path(PROXY_INFO_PATH), "proxy.json"),
         ],
         trusted_host_info[InstanceInfo.ID.value],
-        True,
     )
 
     bootstrap_instance(
         get_path(KEY_PAIR_PATH),
         proxy_info[InstanceInfo.PUBLIC_IP.value],
         [
-            File(get_path(BOOTSTRAP_PATH), "bootstrap.sh"),
+            File(get_path(APP_BOOTSTRAP_PATH), "bootstrap.sh"),
+            File(get_path(WORKERS_INFO_PATH), "workers.json"),
+            File(get_path(MANAGER_INFO_PATH), "manager.json"),
             File(get_path(PROXY_APP_PATH), "main.py"),
         ],
         proxy_info[InstanceInfo.ID.value],
-        True,
     )
 
-    # TODO: Bootstrap the rest of the apps
+    for worker_info in workers_info:
+        bootstrap_instance(
+            get_path(KEY_PAIR_PATH),
+            worker_info[InstanceInfo.PUBLIC_IP.value],
+            [
+                File(get_path(MYSQL_BOOTSTRAP_PATH), "bootstrap.sh"),
+                File(get_path(MYSQL_WORKER_MAIN_PATH), "main.py"),
+                File(get_path(MYSQL_DEP_PATH), "db.py"),
+            ],
+            worker_info[InstanceInfo.ID.value],
+        )
+    
+    bootstrap_instance(
+            get_path(KEY_PAIR_PATH),
+            manager_info[InstanceInfo.PUBLIC_IP.value],
+            [
+                File(get_path(MYSQL_BOOTSTRAP_PATH), "bootstrap.sh"),
+                File(get_path(MYSQL_MANAGER_MAIN_PATH), "main.py"),
+                File(get_path(MYSQL_DEP_PATH), "db.py"),
+                File(get_path(WORKERS_INFO_PATH), "workers.json"),
+            ],
+            manager_info[InstanceInfo.ID.value],
+        )
