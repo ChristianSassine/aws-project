5a20f0f6f8ca64d91548b5cd4e110982393d1ab1
Christian <71024958+ChristianSassine@users.noreply.github.com>
2024-12-02 05:54:05 -0500

    Update main entrypoint

--- deploy/main.py
+++ deploy/main.py
@@ -3,10 +3,16 @@ from cleanup import cleanup
 import sys
 from constants import *
 import asyncio
+import benchmark
+
+async def deploy():
+    await launch.deploy()
+    await benchmark.launch_requests()
+    cleanup()
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if args and args[0] == "cleanup":
         cleanup()
     elif not args:
-        asyncio.run(launch.deploy())
+        asyncio.run(deploy())
